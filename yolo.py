8f40b620e10b0caf9f2b2b97d2c7ebe5a052a21e
ha-takahashi <ha-takahashi@kddi.com>
2019-05-17 19:49:28 +0900

    Modify img path fot Trial_Asaka

--- yolo.py
+++ yolo.py
@@ -27,9 +27,9 @@ class YOLO(object):
         "iou" : 0.45,
         "model_image_size" : (416, 416),
         "gpu_num" : 1,
-        "testfiles_path" : '/mnt/datasets/aushop_fullbody_tvx1_20190227/Converted_Root/ImageSets/Main/val.txt',
-        "motfile_path"  : '/mnt/datasets/aushop_fullbody_tvx1_20190227/Converted_Root/est_mot.txt',
-        "timefile_path" : '/mnt/datasets/aushop_fullbody_tvx1_20190227/Converted_Root/est_time.txt',
+        "testfiles_path" : '/mnt/datasets/aushop_fullbody_tvx1_20190227/Trial_Asaka/ImageSets/Main/val.txt',
+        "motfile_path"  : '/mnt/datasets/aushop_fullbody_tvx1_20190227/Trial_Asaka/est_mot.txt',
+        "timefile_path" : '/mnt/datasets/aushop_fullbody_tvx1_20190227/Trial_Asaka/est_time.txt',
     }
 
     @classmethod
